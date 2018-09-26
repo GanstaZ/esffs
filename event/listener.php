df242216c16275fa43a45aaf3a45f5c08f4a9f20
GanstaZ <ganstaz@hotmail.com>
2018-09-26 14:11:00 +0300

    Update & fix

--- event/listener.php
+++ event/listener.php
@@ -57,8 +57,32 @@ class listener implements EventSubscriberInterface
 	public static function getSubscribedEvents()
 	{
 		return [
+			'core.acp_board_config_edit_add' => 'board_config_add',
 			'core.index_modify_page_title' => 'modify_stats',
 		];
+	}
+
+	/**
+	* Event core.acp_board_config_edit_add
+	*
+	* @param \phpbb\event\data $event The event object
+	*/
+	public function board_config_add($event)
+	{
+		if ($event['mode'] === 'settings')
+		{
+			$this->language->add_lang('esffs_acp', 'ganstaz/esffs');
+
+			$set_data = $event['display_vars'];
+
+			$set_data['vars']['esffs_exclude_ids'] = [
+				'lang' => 'ESFFS_EXCLUDE_IDS',
+				'validate' => 'string', 'type' => 'text:40:255',
+				'explain' => true
+			];
+
+			$event['display_vars'] = $set_data;
+		}
 	}
 
 	/**
@@ -68,27 +92,54 @@ class listener implements EventSubscriberInterface
 	*/
 	public function modify_stats($event)
 	{
-		$sql = 'SELECT forum_id, forum_posts_approved, forum_topics_approved
-				FROM ' . FORUMS_TABLE . '
-				WHERE ' . $this->db->sql_in_set('forum_id', [2, 4]);
-		$result = $this->db->sql_query($sql);
+		$exc_ops = $this->config['esffs_exclude_ids'];
 
-		$hidden = [
-			'posts'	 => (int) $this->config['num_posts'],
-			'topics' => (int) $this->config['num_topics'],
-		];
-
-		while ($row = $this->db->sql_fetchrow($result))
+		// If strings is empty ('' or 0), then stop the process.
+		if (!$exc_ops)
 		{
-			$hidden['posts']  -= (int) $row['forum_posts_approved'];
-			$hidden['topics'] -= (int) $row['forum_topics_approved'];
+			return;
 		}
-		$this->db->sql_freeresult($result);
 
-		// Assign index specific vars
-		$this->template->assign_vars([
-			'TOTAL_POSTS'  => $this->language->lang('TOTAL_POSTS_COUNT', $hidden['posts']),
-			'TOTAL_TOPICS' => $this->language->lang('TOTAL_TOPICS', $hidden['topics']),
-		]);
+		// Check spaces in a string and remove if any.
+		if (strpos($exc_ops, ' ') !== false)
+		{
+			$exc_ops = str_replace(' ', '', $exc_ops);
+		}
+
+		$exclude_ids_ary = [];
+		// Convert string into an array and assign validated ids into exclude_ids_ary.
+		foreach (explode(',', $exc_ops) as $e_id)
+		{
+			if (ctype_digit($e_id) && $e_id > 1)
+			{
+				$exclude_ids_ary[] = $e_id;
+			}
+		}
+
+		if ($exclude_ids_ary)
+		{
+			$sql = 'SELECT forum_id, forum_posts_approved, forum_topics_approved
+					FROM ' . FORUMS_TABLE . '
+					WHERE ' . $this->db->sql_in_set('forum_id', $exclude_ids_ary);
+			$result = $this->db->sql_query($sql);
+
+			$hidden = [
+				'posts'	 => (int) $this->config['num_posts'],
+				'topics' => (int) $this->config['num_topics'],
+			];
+
+			while ($row = $this->db->sql_fetchrow($result))
+			{
+				$hidden['posts']  -= (int) $row['forum_posts_approved'];
+				$hidden['topics'] -= (int) $row['forum_topics_approved'];
+			}
+			$this->db->sql_freeresult($result);
+
+			// Update index specific vars
+			$this->template->assign_vars([
+				'TOTAL_POSTS'  => $this->language->lang('TOTAL_POSTS_COUNT', $hidden['posts']),
+				'TOTAL_TOPICS' => $this->language->lang('TOTAL_TOPICS', $hidden['topics']),
+			]);
+		}
 	}
 }

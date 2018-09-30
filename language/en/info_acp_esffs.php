65583b99c223c8ec8ddd5d49e9d04a15bba9b1e1
GanstaZ <ganstaz@hotmail.com>
2018-09-30 20:31:32 +0300

    remove unneeded language vars

--- language/en/info_acp_esffs.php
+++ language/en/info_acp_esffs.php
@@ -19,8 +19,6 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, [
-	'ESFFS_ENABLE'				=> 'Enable ESFFS',
-	'ESFFS_ENABLE_EXPLAIN'		=> 'If set to <strong>Yes</strong>, selected forums will be excluded from (index etc.) statistics.',
 	'ACP_ESFFS_LEGEND'			=> 'Esffs settings',
 	'ACP_ESFFS_EXCLUDE'			=> 'Exclude forum',
 	'ACP_ESFFS_EXCLUDE_EXPLAIN' => 'Set <strong>Yes</strong> to exclude this forum from (index etc.) statistics.',
